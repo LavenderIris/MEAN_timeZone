ebab664d92c898aa78fcafd5a90a78706b9b5cdb
LavenderIris <pchung@gmail.com>
2017-11-02 18:36:02 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'US Time Zone Display';
+  time: any;
+  isClear = false;
+
+  get_PST() {
+    this.isClear = false;
+    this.time = new Date();
+  }
+
+  get_EST() {
+    this.isClear = false;
+    this.time = new Date();
+    this.time.setHours(this.time.getHours() + 3);
+  }
+
+  get_CST() {
+    this.isClear = false;
+    this.time = new Date();
+    this.time.setHours(this.time.getHours() + 1);
+  }
+
+  get_MST() {
+    this.isClear = false;
+    this.time = new Date();
+    this.time.setHours(this.time.getHours() + 2);
+  }
+
+  clear() {
+    this.isClear = true;
+  }
 }
